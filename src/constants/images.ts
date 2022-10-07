65e3e83d0043be113cd0ad707c1a30948ed0d32b
Steve <steveanil2003@gmail.com>
2022-10-06 21:09:01 -0700

    feat: assets

---
+++ src/constants/images.ts
@@ -0,0 +1,17 @@
+import googleLogo from '../assets/googleLogo.png';
+import logo from '../assets/logo.png'
+import navbarLogo from '../assets/navbarLogo.png'
+import navbarLogo1 from '../assets/navbarLogo1.png'
+import navbarLogo2 from '../assets/navbarLogo2.png'
+import navbarLogo3 from '../assets/navbarLogo3.png'
+import navbarLogo4 from '../assets/navbarLogo4.png'
+
+export default {
+    googleLogo,
+    logo,
+    navbarLogo,
+    navbarLogo1,
+    navbarLogo2,
+    navbarLogo3,
+    navbarLogo4,
+};
