d8f7c76ebb8e34bef9c875990ddf7cc7c7b4861a
Steve <steveanil2003@gmail.com>
2022-10-06 21:21:51 -0700

    chore: update color util

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      'royal-blue': '#17335b',
+      'light-blue': '#3fa3da',
+      'orange': '#fcb814',
+      'red': '#e92b2c',
+    },
+
     extend: {
       height: {
         Header: "80px",
@@ -11,6 +18,8 @@ module.exports = {
         TeamContainer: "350px",
         AwardsContainer: "510px",
         Footer: "310px",
+        logo: "55px",
+        logoWidth: "55px"
       },
     },
   },
